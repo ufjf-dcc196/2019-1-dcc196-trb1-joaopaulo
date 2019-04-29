2c010757404d4d0d33f54a557a1350bc4b57b361
Pedro Freitas <soufreitas.pedro@gmail.com>
2019-04-28 21:22:55 -0300

    Criando activity de disciplina

--- app/src/main/java/com/ufjf/br/trabalho1/activity/DisciplinaActivity.java
+++ app/src/main/java/com/ufjf/br/trabalho1/activity/DisciplinaActivity.java
@@ -1,6 +1,38 @@
 package com.ufjf.br.trabalho1.activity;
 
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ufjf.br.trabalho1.model.Disciplina;
+
 
 public class DisciplinaActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planejamento);
+        Button botaoSalvarDisciplina = findViewById(R.id.buttonSalvarDisciplina);
+        botaoSalvarDisciplina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTitulo = findViewById(R.id.edt_Titulo);
+                EditText editHoras = findViewById(R.id.edt_horas);
+                EditText editArea = findViewById(R.id.edt_Area);
+                String titulo = editTitulo.getText().toString();
+                String area = editArea.getText().toString();
+                Integer horas = Integer.parseInt(editHoras.getText().toString());
+                Disciplina disciplina = new Disciplina(horas, titulo, area);
+                Intent intent = new Intent();
+                intent.putExtra("disciplina", disciplina);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+
+    }
 }
